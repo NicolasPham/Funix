ba993229a6eea1d4ccb0949688701bce98968ecc
Nicolas Pham <67529386+NicolasPham@users.noreply.github.com>
2023-06-08 19:18:36 +0000

    update search - render breed options

--- Javascript/Assignment 02 (Stater)/script/search.js
+++ Javascript/Assignment 02 (Stater)/script/search.js
@@ -16,15 +16,35 @@ const findBtn = document.getElementById("find-btn");
 //Get Data from local Storage
 const petData = getFromStorage("petData");
 let result = [];
-console.log(petData);
 
 function searchID(pet) {
-  if (pet.id.includes(inputID.value)) {
-    result.push(pet);
-    return true;
-  } else return false;
+  //Guard clause: no input ID input will apply to all ID
+  if (inputID.value.length <= 0) {
+    return true
+  }
+    const upperCaseChar = inputID.value.toUpperCase();
+  const lowerCaseChar = inputID.value.toLowerCase();
+  
+  if (pet.id.includes(upperCaseChar) || pet.id.includes(lowerCaseChar)) {
+    return true
+  } else return false
 }
 
+function searchName(pet) {
+  //Guard clause: no input name input will apply to all name
+  if (inputName.value.length <= 0) return true
+
+  const upperCaseChar = inputName.value.toUpperCase();
+  const lowerCaseChar = inputName.value.toLowerCase();
+  
+  if (pet.name.includes(upperCaseChar) || pet.name.includes(lowerCaseChar)) {
+    return true
+  } else return false
+}
+
+function searchType(pet) {
+
+}
 //Render Data
 function renderData() {
   document.getElementById("tbody").innerHTML = "";
@@ -51,15 +71,29 @@ function renderData() {
     document.getElementById("tbody").appendChild(addRow);
   });
 }
+//We need to render breed data so whenver user choose type, breed will show accordingly
+function renderBreed(e) {
+  const breedData = getFromStorage('breed')
+  const filterBreed = breedData.filter(breed => {
+    return breed.type === e.target.value  })
+
+    //clear all privous value
+}
+
+// renderBreed()
 //==================================================
 //The total search function
 function searchPet() {
   result = [];
   petData.forEach((pet, i) => {
-    searchID(pet);
+    
+    if (searchID(pet) && searchName(pet)) {
+      result.push(pet);
+    }
     renderData();
   });
 }
 
 //Add Event Listener
 findBtn.addEventListener("click", searchPet);
+inputType.addEventListener('change', (e) => renderBreed(e))
